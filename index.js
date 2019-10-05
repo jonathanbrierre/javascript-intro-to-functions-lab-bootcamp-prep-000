d93bc9c765745b28e1ab21b052da65d9d5ab48b1
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-05 23:26:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) { 
-   return "I can't hear you!" 
+   if (whisper(string)) {return "I can't hear you!"}
+   else if (shout(string)) {return "YES INDEED" }
    
 }
